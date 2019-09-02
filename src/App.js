9fd03aa000481c0419dca2621e136884360a7bc9
shehbazjafri <galaxytwirls@gmail.com>
2019-09-02 18:59:30 +0530

    Added working previewer with default markdown

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import marked from "marked";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const defaultMD = require("./defaultMarkdown.md");
+  const [markdownText, setMarkdownText] = useState("");
+
+  useEffect(() => {
+    fetch(defaultMD)
+      .then(response => {
+        return response.text();
+      })
+      .then(text => {
+        setMarkdownText(text);
+      });
+  }, [defaultMD]);
+
+  const onTextChange = event => {
+    setMarkdownText(event.target.value);
+  };
+  return (
+    <div className="App">
+      <textarea
+        id="editor"
+        value={markdownText}
+        onChange={onTextChange}
+      ></textarea>
+      <div
+        id="preview"
+        dangerouslySetInnerHTML={{
+          __html: marked(markdownText, { gfm: true, breaks: true })
+        }}
+      ></div>
+    </div>
+  );
 }
 
 export default App;
